4672ba451d7c45a4fb915d8fd4799d70bcc11fd2
AshNlandofOzz <ashleyosburn88@gmail.com>
2022-10-07 17:42:21 -0600

    Full stack tst

--- src/main/java/com/skilldistillery/film/data/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAO.java
@@ -1,5 +1,9 @@
 package com.skilldistillery.film.data;
 
-public interface FilmDAO {
+import java.sql.SQLException;
 
+import com.skilldistillery.film.entities.Film;
+
+public interface FilmDAO {
+	Film getFilmById(int filmId);
 }
--- src/main/java/com/skilldistillery/film/data/FilmDaoJdbcImpl.java
+++ src/main/java/com/skilldistillery/film/data/FilmDaoJdbcImpl.java
@@ -1,8 +1,71 @@
 package com.skilldistillery.film.data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.stereotype.Component;
+
+import com.skilldistillery.film.entities.Film;
 
 @Component
 public class FilmDaoJdbcImpl implements FilmDAO {
+
+	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=US/Mountain";
+	private String user = "student";
+	private String pass = "student";
+
+	@Override
+	public Film getFilmById(int filmId) {
+		Film film = null;
+
+		try {
+			Connection conn = DriverManager.getConnection(URL, user, pass);
+
+			String sql = "SELECT film.id, film.title, film.description, film.release_year, "
+					+ "film.language_id, film.rental_duration, film.rental_rate, film.length, "
+					+ "film.replacement_cost, film.rating, film.special_features,  language.name "
+					+ "FROM film JOIN language ON film.language_id = language.id " + "WHERE film.id = ?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, filmId);
+
+			ResultSet filmResult = stmt.executeQuery();
+
+			if (filmResult.next()) {
+				film = new Film();
+				film.setId(filmResult.getInt("id"));
+				film.setTitle(filmResult.getString("Title"));
+				film.setDescription(filmResult.getString("description"));
+				film.setReleaseYear(filmResult.getString("release_year"));
+				film.setLanguageId(filmResult.getInt("language_id"));
+				film.setRentalDuration(filmResult.getInt("rental_duration"));
+				film.setRentalRate(filmResult.getDouble("rental_rate"));
+				film.setLength(filmResult.getDouble("length"));
+				film.setReplacementCost(filmResult.getDouble("replacement_cost"));
+				film.setRating(filmResult.getString("rating"));
+				film.setSpecialFeatures(filmResult.getString("special_features"));
+				film.setLanguage(filmResult.getString("name"));
+
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return film;
+
+	}
+
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("Error loading MySQL Driver");
+			throw new RuntimeException("Unable to load MySQL Driver class");
+		}
+	}
 
 }
